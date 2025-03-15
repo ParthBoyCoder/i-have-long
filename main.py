fff888bfd54c60665afcf77dc5e0822e75161b18
parth <90976533+ParthBoyCoder@users.noreply.github.com>
2025-03-15 19:17:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def mail(content):
     # Email Configuration
     sender_email = "parthib.soursh@gmail.com"
     receiver_email = "banerjeeparthib63@gmail.com"
-    password = "dwck wrib vtos tobk"  # Use an App Password if using Gmail
+    password = ""  # Use an App Password if using Gmail
 
     # Create Email
     msg = EmailMessage()
